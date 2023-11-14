1df37294ee30ea667b76c72eb2c91efd9e4c0045
lucasdelimagarreto <54108848+lucasdelimagarreto@users.noreply.github.com>
2023-11-14 14:49:36 -0300

    Game seed

--- dslist/src/main/java/com/lucasdevsuperior/dslist/entities/Game.java
+++ dslist/src/main/java/com/lucasdevsuperior/dslist/entities/Game.java
@@ -19,19 +19,25 @@ public class Game {
     @Column(name = "game_year")
     private int year;
     private String genre;
+    private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){}
 
-    public Game(Long id, String title, int year, String genre, Double score, String imgUrl, String shortDescription,
-            String longDescription) {
+    public Game(Long id, String title, int year, String genre, String platforms, Double score, String imgUrl,
+            String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -69,6 +75,15 @@ public class Game {
     public void setGenre(String genre) {
         this.genre = genre;
     }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
 
     public Double getScore() {
         return score;
