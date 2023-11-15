9dff7cdfa154a6b95175f473b426ff1d7610604b
lucasdelimagarreto <54108848+lucasdelimagarreto@users.noreply.github.com>
2023-11-15 17:17:50 -0300

    Domain model

---
+++ dslist/src/main/java/com/lucasdevsuperior/dslist/entities/Belongin.java
@@ -0,0 +1,64 @@
+package com.lucasdevsuperior.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belongin")
+public class Belongin {
+    
+    @EmbeddedId
+    private BelonginPK id = new BelonginPK();
+
+    private Integer position;
+
+    public Belongin (){}
+
+    public Belongin(Game game, GameList list, Integer position) {
+        this.id.setGame(game);
+        this.id.setList(list);
+        this.position = position;
+    }
+    
+    public BelonginPK getId() {
+        return id;
+    }
+
+    public void setId(BelonginPK id) {
+        this.id = id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Belongin other = (Belongin) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
