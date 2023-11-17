f28bd99719a40ba38b86e22a6db276530811496b
lucasdelimagarreto <54108848+lucasdelimagarreto@users.noreply.github.com>
2023-11-17 17:43:10 -0300

    Homologação feita

--- dslist/src/main/java/com/lucasdevsuperior/dslist/dto/GameMinDTO.java
+++ dslist/src/main/java/com/lucasdevsuperior/dslist/dto/GameMinDTO.java
@@ -24,7 +24,7 @@ public class GameMinDTO {//Data Transfer Objects
     public GameMinDTO(GameMinProjection projection) {
         id = projection.getId();
         title = projection.getTitle();
-        year = projection.getYear();
+        year = projection.getGameYear();
         imgUrl = projection.getImgUrl();
         shortDescription = projection.getShortDescription();
     }
